2b7636f96a351592c2ca11bf97f4c587c187a71a
caetanomarcus <caetanomarcusfgmail.com>
2021-05-21 17:56:15 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { useState} from 'react'
+
+function Home(){
+    return (
+        <header>
+            <h1>Home</h1>
+            <nav>
+                <ul>
+                    <li>Início</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+
+
+
+export default Home;
